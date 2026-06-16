30808027394f14c1e8a8b8b0b167fc818a05b842
Nasril-Ilham <nasrilham30@gmail.com>
2026-06-16 22:07:43 +0700

    feat: response json simple

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,33 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    
+       protected function response(
+        string $message,
+        mixed $data = null,
+        int $status = 200
+    ) {
+        return response()->json([
+            'message' => $message,
+            'data' => $data
+        ], $status);
+    }
 }
+
+// cara pake di atas
+
+//  public function index()
+//     {
+//         $penjualan = penjualanmodel::all();
+
+//         return $this->response(
+//          // pertama menampung massage
+//          'data tersampaikan'
+//     
+//          // menampung data
+//          // $penjualan
+//
+//          // menampung code http
+//          //  200 / 201 / 404
+//         );
+
+//     }
